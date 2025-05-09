d250a656c628489ce76b66ef5b3ddba68d39f5b3
Isaac Jerish <152412476+isaacjerish@users.noreply.github.com>
2025-05-08 23:05:39 -0400

    INtermediate changes

--- assembler/assembler.py
+++ assembler/assembler.py
@@ -3,13 +3,25 @@ import struct
 import re
 
 OPCODE_TABLE = {
-    0x01: "MOV",
-    0x02: "ADD",
-    0x03: "SUB",
-    0x04: "CMP",
-    0x05: "JE",
-    0xFF: "HLT",
+    "MOV": 0x01,
+    "ADD": 0x02,
+    "SUB": 0x03,
+    "CMP": 0x04,
+    "JE": 0x05,
+    "HLT": 0xFF,
 }
-
-with open("test.asm", "r") as f:
-    lines = f.readlines
+REGISTER_TABLE = {
+    "R0": 0x00,
+    "R1": 0x01,
+    "R2": 0x02,
+    "R3": 0x03,
+    "R4": 0x04,
+    "R5": 0x05,
+    "R6": 0x06,
+    "R7": 0x07,
+}
+with open("/Users/isaacjerish/Documents/ViperVM/programs/test.asm", "r") as f:
+    lines = f.readlines()
+    for line in lines:
+        
+    print(lines)
